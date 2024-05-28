85ee9a967c96a4c7d03a1937d1947f77dc53f994
AliceHeeyeon <270275774@yoobeestudent.ac.nz>
2024-05-28 13:02:30 +1200

    link to github and figma

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaFigma } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -14,10 +15,15 @@ const Footer = () => {
         </ul>
      </div>
      <div className='footer-social'>
-        <FaLinkedin />
-        <FaGithub />
-        <FaFigma />
-
+         <Link to='https://www.linkedin.com/in/alice-heeyeon-kim/'>
+            <FaLinkedin />
+         </Link>
+         <Link to='https://github.com/AliceHeeyeon/ottgeeks'>
+            <FaGithub />
+         </Link>
+         <Link to='https://www.figma.com/design/Trc0KBPhlPyYmaPCTye378/ottgeeks?node-id=0-1&t=Xv6ODoukVw8t7ClA-1'>
+            <FaFigma />
+         </Link>
      </div> 
      <div className='footer-copyright'>
         <p>©OTTGEEKS. All Rights Reserved</p>
